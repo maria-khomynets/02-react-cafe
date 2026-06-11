2b2af48de94ea6a23fef7fe7f06859c8396d5a4e
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-11 20:59:06 +0300

    commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+//import { useState } from "react";
+import css from "./App.module.css";
+
+export default function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+}
