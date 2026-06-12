e4d9a9c0cb76b990ba525dd9cfed4a24e2950015
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-12 21:31:34 +0300

    add voteststs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,30 @@
-//import { useState } from "react";
+import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import Notification from "../Notification/Notification";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
 import css from "./App.module.css";
+import type { Votes, VoteType } from "../../types/votes";
 
 export default function App() {
-  // const [count, setCount] = useState(0);
-
-  return <CafeInfo />;
-  <Notification />;
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  
+  
+const handleVote = (type: VoteType) => {
+  setVotes({ ...votes, [type]: votes[type] + 1 });
+};
+  const resetVotes = () => setVotes({ good: 0, neutral: 0, bad: 0 });
+  console.log(votes);
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions />
+      <VoteStats />
+      <Notification />;
+    </div>
+  );
 }
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,19 +1,17 @@
 import css from "./VoteOptions.module.css";
-interface VoteStatsProps {
-    votes: {
-        good: number;
-        neutral: number;
-        bad:number;
-    } ;
-totalVotes: number;
-positiveRate: number;
+import { type VoteType } from './../../types/votes';
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
 }
-export default function VoteOptions() {
-     retutn <div className={css.container}>
-  <button className={css.button}>Good</button>
-  <button className={css.button}>Neutral</button>
-  <button className={css.button}>Bad</button>
-  <button className={`${css.button} ${css.reset}`}>Reset</button>
-</div>;
- }
-
+export default function VoteOptions({onVote, onReset, canReset}: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button  className={css.button}>Good</button>
+      <button className={css.button}>Neutral</button>
+      <button className={css.button}>Bad</button>
+      <button className={`${css.button} ${css.reset}`}>Reset</button>
+    </div>
+  );
+}
