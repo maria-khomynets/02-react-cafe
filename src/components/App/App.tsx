e046480f9d8b14a5b4ee9d4de0322754231bc1c1
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-13 12:30:11 +0300

    add homeWork

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,26 +5,44 @@ import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import css from "./App.module.css";
 import type { Votes, VoteType } from "../../types/votes";
-
-export default function App() {
-  const [votes, setVotes] = useState<Votes>({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-  
-  
-const handleVote = (type: VoteType) => {
-  setVotes({ ...votes, [type]: votes[type] + 1 });
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
 };
-  const resetVotes = () => setVotes({ good: 0, neutral: 0, bad: 0 });
-  console.log(votes);
+export default function App() {
+  const [votes, setVotes] = useState<Votes>(initialState);
+
+  function handleVote(type: VoteType) {
+    setVotes({ ...votes, [type]: votes[type] + 1 });
+  }
+
+  function resetVotes() {
+    setVotes(initialState);
+  }
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions />
-      <VoteStats />
-      <Notification />;
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes ? true : false}
+      />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,17 +1,34 @@
 import css from "./VoteOptions.module.css";
-import { type VoteType } from './../../types/votes';
+import { type VoteType } from "./../../types/votes";
 interface VoteOptionsProps {
   onVote: (type: VoteType) => void;
   onReset: () => void;
   canReset: boolean;
 }
-export default function VoteOptions({onVote, onReset, canReset}: VoteOptionsProps) {
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
   return (
     <div className={css.container}>
-      <button  className={css.button}>Good</button>
-      <button className={css.button}>Neutral</button>
-      <button className={css.button}>Bad</button>
-      <button className={`${css.button} ${css.reset}`}>Reset</button>
+      <button onClick={() => onVote("good")} className={css.button}>
+        Good
+      </button>
+      <button onClick={() => onVote("neutral")} className={css.button}>
+        Neutral
+      </button>
+      <button onClick={() => onVote("bad")} className={css.button}>
+        Bad
+      </button>
+      {canReset && (
+        <button
+          onClick={() => onReset()}
+          className={`${css.button} ${css.reset}`}
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 }
